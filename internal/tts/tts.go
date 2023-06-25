3a43074731b3f996b057033e953a65f5d4c90c0f
Yoshiyuki Koyanagi <moutend@gmail.com>
2023-06-25 09:23:12 +0900

    Update

--- internal/tts/tts.go
+++ internal/tts/tts.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"sync"
@@ -57,6 +58,60 @@ func Process(ctx context.Context, ts []token.Token, option ProcessOption) error 
 	return nil
 }
 
+func ProcessOffline(ctx context.Context, ts []token.Token, option ProcessOption) error {
+	commands := make([]*exec.Cmd, 0, len(ts))
+
+	englishVoice := option.EnglishVoice
+	englishRate := fmt.Sprint(option.EnglishRate)
+	japaneseVoice := option.JapaneseVoice
+	japaneseRate := fmt.Sprint(option.JapaneseRate)
+
+	temporaryDir := filepath.Join(option.AudioDir, "temporary")
+
+	os.RemoveAll(temporaryDir)
+	os.MkdirAll(temporaryDir, 0755)
+
+	filePaths := []string{}
+
+	for i, t := range ts {
+		switch t.Kind {
+		case token.Number, token.Symbol:
+			for _, r := range []rune(t.Text) {
+				filePaths = append(filePaths, filepath.Join(option.AudioDir, fmt.Sprintf("%03d.wav", r+1)))
+			}
+		case token.Alphabet:
+			filePath := filepath.Join(temporaryDir, fmt.Sprintf("text%03d.aiff", i))
+			filePaths = append(filePaths, filePath)
+			commands = append(commands, exec.CommandContext(ctx, "say", "-o", filePath, "-v", englishVoice, "-r", englishRate, fmt.Sprintf("%q", t.Text)))
+		case token.Unicode:
+			filePath := filepath.Join(temporaryDir, fmt.Sprintf("text%03d.aiff", i))
+			filePaths = append(filePaths, filePath)
+			commands = append(commands, exec.CommandContext(ctx, "say", "-o", filePath, "-v", japaneseVoice, "-r", japaneseRate, fmt.Sprintf("%q", t.Text)))
+		}
+	}
+
+	var wg sync.WaitGroup
+
+	for i := range commands {
+		wg.Add(1)
+
+		go func(n int) {
+			if err := commands[n].Run(); err != nil {
+				option.Debug.Println("Error:", err)
+			}
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+
+	if err := exec.CommandContext(ctx, "play", filePaths...).Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 type Engine struct {
 	cancelFuncCounter int
 	cancelFuncMap     map[int]context.CancelFunc
@@ -81,7 +136,7 @@ func (e *Engine) Speak(ctx context.Context, tokens []token.Token, option Process
 	e.cancelFuncMap[e.cancelFuncCounter] = cancel
 
 	go option.Debug.Println("Speak function invoked:", e.cancelFuncCounter)
-	go Process(ctx, tokens, option)
+	go ProcessOffline(ctx, tokens, option)
 }
 
 func (e *Engine) Pause(option ProcessOption) {
